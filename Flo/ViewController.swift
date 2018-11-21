cc2b51d1d226d57ebcc9f0d9f960efa783efea9a
Zakhar <zazman1999@gmail.com>
2018-11-21 16:10:21 +0200

    Finished

--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+  @IBOutlet weak var counterView: CounterView!
+  @IBOutlet weak var counterLabel: UILabel!
+  
+  @IBAction func pushButtonPressed(_ button: PushButton) {
+    if button.isAddButton {
+      counterView.counter += 1
+    } else {
+      if counterView.counter > 0 {
+        counterView.counter -= 1
+      }
+    }
+    counterLabel.text = String(counterView.counter)
   }
-
-
 }
 
